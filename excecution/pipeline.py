d2ba92d44980dfd1f75fcdc28996026d713303c0
andresgvelasquez <andresgvelasquez8@gmail.com>
2024-08-15 19:30:26 -0700

    Se añaden funciones para los modelos de machine learning Lightgbm y xgboost.

--- excecution/pipeline.py
+++ excecution/pipeline.py
@@ -3,6 +3,9 @@ from preprocessing.preprocessing import preprocessing_data
 from preprocessing.preparing import preparing_data
 from models.d00_dummy import dummytest
 from models.m00_logistic_regresion import log_reg_model
+from models.c00_catboost import catboost_model
+from models.l00_lightgbm import lgbm_model
+from models.x00_xgboost import xgboost_model
 
 # Lectura de los archivos
 df_contract, df_internet, df_personal, df_phone = read_csv_files('./files/datasets/input/')
@@ -16,7 +19,16 @@ features_train_encoded, features_test_encoded, features_train_encoded_scaled, fe
 # Aplicar los modelos de machine learning
 
 # Dummy model
-dummytest(features_train_encoded, target_train, features_test_encoded, target_test)
+dummytest(features_train_encoded_scaled, target_train, features_test_encoded_scaled, target_test)
 
 # Logistic Regression model
-log_reg_model(features_train_encoded_scaled, target_train, features_test_encoded_scaled, target_test, show_metrics=True)
+log_reg_model(features_train_encoded_scaled, target_train, features_test_encoded_scaled, target_test)
+
+# Catboost model
+catboost_model(features_train_encoded_scaled, target_train, features_test_encoded_scaled, target_test)
+
+# LightGBM model
+lgbm_model(features_train_encoded_scaled, target_train, features_test_encoded_scaled, target_test)
+
+# XGBoost model
+xgboost_model(features_train_encoded_scaled, target_train, features_test_encoded_scaled, target_test)
